5d4b614d674a67ea31362928bd1587490fa10503
Jax-rgb <jacquescarigma@gmail.com>
2026-03-03 11:25:14 +0800

    Delete UnliCallTextPackage.java

--- UnliCallTextPackage.java
+++
@@ -1,8 +0,0 @@
-// UnliCallTextPackage.java (Concrete Visitor for unli package)
-public class UnliCallTextPackage implements UnliCallOffer {
-    @Override
-    public String showUnliCallsTextOffer(Telco telco) {
-        String status = telco.getUnliCallText() ? "Yes" : "No";
-        return telco.getTelcoName() + " - Unlimited calls and text: " + status;
-    }
-}
