62138206ebab8bcaac40fe730df0d447cc5485bb
Jax-rgb <jacquescarigma@gmail.com>
2026-03-03 11:23:31 +0800

    Delete TelcoAllowance.java

--- TelcoAllowance.java
+++
@@ -1,8 +0,0 @@
-// TelcoAllowance.java (Concrete Visitor for data promo)
-public class TelcoAllowance implements UsagePromo {
-    @Override
-    public String showAllowance(Telco telco) {
-        return telco.getTelcoName() + " - Data: " + telco.getDataAllowance() +
-               "GB @ ₱" + telco.getPromoPrice();
-    }
-}
