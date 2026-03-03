c23e64be95ac194155248eb3d39a56a7f153c0e3
Jax-rgb <jacquescarigma@gmail.com>
2026-03-03 11:24:38 +0800

    Delete TelcoSubscription.java

--- TelcoSubscription.java
+++
@@ -1,9 +0,0 @@
-// TelcoSubscription.java (the Visitable / Element interface)
-public interface TelcoSubscription {
-    String getTelcoName();
-    int getPromoPrice();           // note: int, not double
-    boolean getUnliCallText();
-    // accept method - core of visitor pattern
-    String accept(UsagePromo promo);
-    String accept(UnliCallOffer unli);
-}
